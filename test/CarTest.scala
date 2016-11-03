58e36b890d8ab1964205a29aece56272e0e57474
Timon Loeffen <timon.loeffen@gmail.com>
2016-11-03 19:46:30 +0100

    refactor tests for new cars

--- test/CarTest.scala
+++ test/CarTest.scala
@@ -3,40 +3,44 @@ import models._
 
 class CarTest extends PlaySpec {
 
-  "A brand new car" must {
-    "be constructable with all required fields" in {
-      val car = new BrandNewCar("1", "super nice car", Fuel.Gasoline, 5000)
-      car.fuel mustBe Fuel.Gasoline
-      car.id mustBe "1"
-      car.title mustBe "super nice car"
-      car.price mustBe 5000
+  type CarArgs = Tuple4[String, String, Fuel.Type, Int]
+  type CarBuilder = CarArgs => BrandNewCar
+  type UnconstructableTestCase = Tuple2[String, CarArgs]
+
+  val validInput = ("1", "super nice car", Fuel.Gasoline, 5000)
+
+  "A brand new car" when {
+
+    val carBuilder = BrandNewCar.tupled
+
+    "input is valid" must {
+      "be constructable" in {
+        val car = carBuilder(validInput)
+        car.fuel mustBe Fuel.Gasoline
+        car.id mustBe "1"
+        car.title mustBe "super nice car"
+        car.price mustBe 5000
+      }
     }
 
-    "fail on null or empty id" in {
-      List(null, "").foreach { id =>
-        a [IllegalArgumentException] must be thrownBy {
-          new BrandNewCar(id, "title", Fuel.Diesel, 0)
+    notConstructable(carBuilder)
+  }
+
+  def notConstructable(maker: CarBuilder) = {
+    Seq(
+      ("id is null", validInput.copy(_1 = null)),
+      ("id is empty", validInput.copy(_1 = "")),
+      ("title is null", validInput.copy(_2 = null)),
+      ("title is empty", validInput.copy(_2 = "")),
+      ("fuel is null", validInput.copy(_3 = null)),
+      ("price is negative", validInput.copy(_4 = -1))
+    ).foreach { case (condition, args) =>
+      condition must {
+        "not NOT be constructable" in {
+          a[IllegalArgumentException] must be thrownBy {
+            maker(args)
+          }
         }
-      }
-    }
-
-    "fail on null or empty title" in {
-      List(null, "").foreach { title =>
-        a [IllegalArgumentException] must be thrownBy {
-          new BrandNewCar("1", title, Fuel.Diesel, 0)
-        }
-      }
-    }
-
-    "fail on null fuel" in {
-      a [IllegalArgumentException] must be thrownBy {
-        new BrandNewCar("1", "title", null, 0)
-      }
-    }
-
-    "fail on negative price" in {
-      a [IllegalArgumentException] must be thrownBy {
-        new BrandNewCar("1", "title", Fuel.Gasoline, -1)
       }
     }
   }
