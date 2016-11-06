bb0fd0d99d3f55d57dcc6c7c09624b86a34e1c4d
Timon Loeffen <timon.loeffen@gmail.com>
2016-11-06 20:11:44 +0100

    refactor integration test

--- test/IntegrationTest.scala
+++ test/IntegrationTest.scala
@@ -1,13 +1,9 @@
 import models.{AnormCarRepository, CarRepository}
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
-import play.api.Logger
-import play.api.db.Databases
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json._
-import play.api.test.FakeApplication
 import play.api.test.Helpers._
-import play.api.db.evolutions._
 
 class IntegrationTest extends PlaySpec with OneServerPerSuite {
 
@@ -17,28 +13,39 @@ class IntegrationTest extends PlaySpec with OneServerPerSuite {
     .build()
 
   "The car advert api" must {
-    "create a new advert and make it available" in {
 
-      val newCarJson: JsValue = Json.parse(
-        """
-           { "title": "test advert",
-             "fuel": "Gasoline",
-             "price": 123456,
-             "new": true
-           }
-        """)
-      val response = await(wsCall(controllers.routes.CarController.create()).post(
-        newCarJson))
+    val advertJson: JsValue = Json.parse(
+      """
+         { "title": "test advert",
+           "fuel": "Gasoline",
+           "price": 123456,
+           "new": true
+         }
+      """)
+
+    "support all required operations" in {
+      val advertUrl = createAdvert()
+      assertAdvertRetrievable(advertUrl)
+    }
+
+    def createAdvert() = {
+      val response = await(
+        wsCall(
+          controllers.routes.CarController.create()
+        ).post(advertJson))
       response.status mustBe 201
       val location = response.header("location")
       location.value must endWith regex """/car/\d+"""
+      location.value
+    }
 
-      val JsSuccess(carWithId,_) = newCarJson.transform(
-        __.read[JsObject].map( o => o ++ Json.obj("id" -> 1))
+    def assertAdvertRetrievable(url: String) {
+      val JsSuccess(carWithId, _) = advertJson.transform(
+        __.read[JsObject].map(o => o ++ Json.obj("id" -> 1))
           andThen ((__ \ 'new).json.prune)
       )
 
-      val car = await(wsUrl(location.value).get).body
+      val car = await(wsUrl(url).get).body
       Json.parse(car) mustEqual carWithId
     }
   }
